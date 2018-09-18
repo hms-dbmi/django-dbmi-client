bb704e9f5e9ea65cbb6fa0ceb89a7ab1483fc71b
Bryan Larson <b32147@gmail.com>
2018-09-18 08:56:39 -0400

    Updated settings to define URLs and configs for prod and dev environments; requires those configs be set in non-prod/dev environments

--- dbmi_client/settings.py
+++ dbmi_client/settings.py
@@ -10,19 +10,30 @@ from django.conf import settings
 # Don't import directly CONFIG or PANELs, or you will miss changes performed
 # with override_settings in tests.
 
+# Set service URLs based on environment
+DBMI_ENVIRONMENTS = {
+    'prod': {
+        'AUTHN_URL': 'https://authentication.dbmi.hms.harvard.edu',
+        'AUTHZ_URL': 'https://authorization.dbmi.hms.harvard.edu',
+        'REG_URL': 'https://registration.dbmi.hms.harvard.edu',
+        'JWT_AUTHZ_NAMESPACE': 'https://authorization.dbmi.hms.harvard.edu',
+    },
+    'dev': {
+        'AUTHN_URL': 'https://authentication.aws.dbmi-dev.hms.harvard.edu',
+        'AUTHZ_URL': 'https://authorization.aws.dbmi-dev.hms.harvard.edu',
+        'REG_URL': 'https://registration.aws.dbmi-dev.hms.harvard.edu',
+        'JWT_AUTHZ_NAMESPACE': 'https://authorization.aws.dbmi-dev.hms.harvard.edu',
+    }
+}
+
 CONFIG_DEFAULTS = {
     # Client options, assume production environment
-    'ENVIRONMENT': 'production',
-    'AUTHN_URL': 'https://authentication.dbmi.hms.harvard.edu',
-    'AUTHZ_URL': 'https://authorization.dbmi.hms.harvard.edu',
-    'REG_URL': 'https://registration.dbmi.hms.harvard.edu',
+    'ENVIRONMENT': 'prod',
     'AUTHZ_ADMIN_GROUP': 'dbmi-admin',
     'AUTHZ_USER_GROUP': 'dbmi-user',
-    'JWT_AUTHZ_NAMESPACE': 'https://authorization.dbmi.hms.harvard.edu',
     'JWT_HTTP_PREFIX': 'JWT ',
     'JWT_COOKIE_NAME': 'DBMI_JWT',
     'JWT_COOKIE_DOMAIN': '.dbmi.hms.harvard.edu',
-    'AUTHN_LOGO_URL': 'https://authentication.dbmi.hms.harvard.edu/static/hms_shield.png',
     'AUTH0': {
         'CLIENT_IDS': ['!!! must be configured by client !!!'],
         'DOMAIN': 'dbmiauth.auth0.com',
@@ -47,9 +58,31 @@ if 'CLIENT_IDS' not in CLIENT_CONFIG['AUTH0']:
 if 'DOMAIN' not in CLIENT_CONFIG['AUTH0']:
     raise AttributeError('AUTH0.DOMAIN configuration must be set')
 
-if 'ENVIRONMENT' in CLIENT_CONFIG and CLIENT_CONFIG['ENVIRONMENT'] != 'production':
+# Ensure environment is set and if not prod or dev, ensure service URLs are provided
+if 'ENVIRONMENT' in CLIENT_CONFIG and \
+        CLIENT_CONFIG['ENVIRONMENT'].lower() != 'prod' and CLIENT_CONFIG['ENVIRONMENT'].lower() != 'dev':
     warnings.warn(
-        "ENVIRONMENT is not set to production, this should be a test or dev environment", ResourceWarning)
+        "ENVIRONMENT is not set to production, this should be a test environment", ResourceWarning)
+
+    missing_urls = []
+    if 'AUTHN_URL' not in CLIENT_CONFIG:
+        missing_urls.append('AUTHN_URL')
+    if 'AUTHZ_URL' not in CLIENT_CONFIG:
+        missing_urls.append('AUTHZ_URL')
+    if 'REG_URL' not in CLIENT_CONFIG:
+        missing_urls.append('REG_URL')
+    if missing_urls:
+        raise AttributeError('{} configuration(s) must be set'.format(missing_urls))
+
+    if 'JWT_AUTHZ_NAMESPACE' not in CLIENT_CONFIG:
+        warnings.warn(
+            "JWT_AUTHZ_NAMESPACE is not set, JWT claims will not be inspected "
+            "for groups/roles/permissions", ResourceWarning)
+
+else:
+
+    # Update the client config with pre-defined environment URLs, etc
+    CLIENT_CONFIG.update(DBMI_ENVIRONMENTS[CLIENT_CONFIG['ENVIRONMENT']])
 
 if 'JWT_HTTP_PREFIX' in CLIENT_CONFIG:
     warnings.warn(
