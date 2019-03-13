92e2fc96789d25c05a43507d0f2a952ceb33c5b6
semantic-release <semantic-release>
2019-03-13 15:30:14 +0000

    0.3.2
    Automatically generated by python-semantic-release

--- dbmi_client/__init__.py
+++ dbmi_client/__init__.py
@@ -3,7 +3,7 @@ DBMI Client
 """
 
 __title__ = 'DBMI Client'
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 __author__ = 'Bryan Larson'
 __license__ = 'BSD 2-Clause'
 __copyright__ = 'Copyright 2011-2018 Harvard Medical School Department of Biomedical Informatics'
