8ba7953cd7d5978f4cf4e229936c7ec657a042c4
Bryan Larson <bryan_larson@hms.harvard.edu>
2021-02-21 13:58:12 +0000

    chore(release): 0.4.3 [skip ci]
    ## [0.4.3](https://github.com/hms-dbmi/django-dbmi-client/compare/v0.4.2...v0.4.3) (2021-02-21)
    
    ### Bug Fixes
    
    * **authn/support:** PPM-690 - Fixed 401s for AJAX; fixed Support email send ([a8e46bb](https://github.com/hms-dbmi/django-dbmi-client/commit/a8e46bb0e424e91b28b3c83a09a04c669d517885))

--- dbmi_client/__init__.py
+++ dbmi_client/__init__.py
@@ -3,7 +3,7 @@ DBMI Client
 """
 
 __title__ = "DBMI Client"
-__version__ = "0.4.2"
+__version__ = "0.4.3"
 __author__ = "Bryan Larson"
 __license__ = "BSD 2-Clause"
 __copyright__ = "Copyright 2011-2020 Harvard Medical School Department of Biomedical Informatics"
