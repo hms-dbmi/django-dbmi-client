82adc67534b7a0af6a2064eba73d965cfbc3c28f
Bryan Larson <bryan_larson@hms.harvard.edu>
2024-07-18 07:31:04 -0600

    feat(reporting): Added release version to Sentry initialization; sets environment

--- dbmi_client/reporting.py
+++ dbmi_client/reporting.py
@@ -1,8 +1,22 @@
 import sentry_sdk
 from furl import furl
 
+from dbmi_client.settings import dbmi_settings
 
-def sentry(sentry_dsn, sentry_trace_rate=0.0, sentry_profile_rate=0.0):
+
+def sentry(sentry_dsn: str, release: str = None, sentry_trace_rate: float = 0.0, sentry_profile_rate: float = 0.0):
+    """
+    Initializes the Sentry client for the current application.
+
+    :param sentry_dsn: The DSN for the Sentry project
+    :type sentry_dsn: str
+    :param release: The current release of the application, defaults to None
+    :type release: str, optional
+    :param sentry_trace_rate: The rate at which traces are sent, defaults to 0.0
+    :type sentry_trace_rate: float, optional
+    :param sentry_profile_rate: The rate at which requests are profiled, defaults to 0.0
+    :type sentry_profile_rate: float, optional
+    """    
 
     def filter_transactions(event, hint):
         parsed_url = furl(event["request"]["url"])
@@ -25,4 +39,8 @@ def sentry(sentry_dsn, sentry_trace_rate=0.0, sentry_profile_rate=0.0):
         # of sampled transactions.
         # We recommend adjusting this value in production.
         profiles_sample_rate=sentry_profile_rate,
+        # Set the environment
+        environment=dbmi_settings.ENVIRONMENT,
+        # Set release, if passed
+        release=release,
     )
